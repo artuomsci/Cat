bcf78dbddcaa76da7b248ffbb813537bfe6e43e8
artuom <artuomsci@gmail.com>
2022-06-13 12:19:22 +0300

    html export

--- utils.h
+++ utils.h
@@ -2,17 +2,83 @@
 
 #include "cat.h"
 
+#include <fstream>
+
 namespace cat
 {
    /**
     * @brief Export data to Cytoscape
     * @param cat_ - export category
+    * @param path_ - export path
     * @param skip_identity_ - flag to skip identity morphisms
     * @param show_morphisms_ - flag to show morphisms as objects
-    * @return Cytoscape string data representation
     */
-   inline std::string export_cytoscape(const Cat& cat_, bool skip_identity_ = true, bool show_morphisms_ = false)
+   inline void export_cytoscape(const Cat& cat_, const std::string& path_, bool skip_identity_ = true, bool show_morphisms_ = false)
    {
+      std::string srctemplate = R"(<!DOCTYPE>
+            <html>
+
+              <head>
+                <title>Graph</title>
+                <script src="cytoscape.min.js"></script>
+
+                <style>
+                  #cy {
+                  height: 100%;
+                  width: 100%;
+                  position: absolute;
+                  left: 0;
+                  top: 0;
+                  float: left;
+                  }
+                </style>
+
+              </head>
+
+              <body>
+                <div id="cy"></div>
+                <script>
+                        let data = {};
+                        var cy = cytoscape({
+                            container: document.getElementById('cy'),
+                            elements: data,
+                            style: [
+                                    {
+                                    selector: 'node[type="Obj"][name]',
+                                       style:
+                                       {
+                                          'content': 'data(name)'
+                                       }
+                                    },
+                                    {
+                                    selector: 'node[type="Morph"][name]',
+                                       style:
+                                       {
+                                          'content': 'data(name)',
+                                          'text-valign': 'center',
+                                          'text-halign': 'center',
+                                          'background-color': 'white'
+                                       }
+                                    },
+                                    {
+                                    selector: 'edge',
+                                       style:
+                                       {
+                                          'curve-style': 'bezier',
+                                          'target-arrow-shape': 'triangle'
+                                       }
+                                    }
+                            ],
+                            layout: {
+                                name: 'breadthfirst'
+                            }
+                        });
+
+                </script>
+              </body>
+
+            </html>)";
+
       std::string nodes;
 
       for (const auto& [obj, objset] : cat_.GetObjects())
@@ -80,6 +146,19 @@ namespace cat
          }
       }
 
-      return "nodes: [" + nodes + "]" + "," + "edges: [" + edges + "]";
+      // aggregate data into string
+      std::string data = "nodes: [" + nodes + "]" + "," + "edges: [" + edges + "]";
+
+      // filling template
+      auto ind = srctemplate.find("{}");
+      srctemplate.insert(ind + 1, data);
+
+      // file dumping
+      std::ofstream file(path_ + "/index.html", std::ofstream::out);
+      if (file.is_open())
+      {
+         file << srctemplate;
+         file.close();
+      }
    }
 }
