37d48feb66f477b475e336afa379bca023482fdd
artuom <artuomsci@gmail.com>
2023-07-09 14:19:29 +0300

    leftovers

--- test/equivalence.h
+++ test/equivalence.h
@@ -81,6 +81,39 @@ namespace cat
 
          assert(!arrow_left.IsEquivalent(arrow_right));
       }
+
+      {
+         Arrow arrow_left("A", "B");
+         Arrow arrow_right("A", "B");
+
+         Arrow arrow_0("a0", "b0");
+
+         arrow_0.EmplaceArrow("x", "y", "first");
+         arrow_0.EmplaceArrow("x", "y", "second");
+
+         arrow_left.AddArrow(arrow_0);
+         arrow_right.AddArrow(arrow_0);
+
+         assert(arrow_left.IsEquivalent(arrow_right));
+      }
+
+      {
+         Arrow arrow_left("A", "B");
+         Arrow arrow_right("A", "B");
+
+         Arrow arrow_00("a0", "b0");
+         arrow_00.EmplaceArrow("x", "y", "first");
+         arrow_00.EmplaceArrow("x", "y", "second");
+
+         Arrow arrow_01("a0", "b0");
+         arrow_01.EmplaceArrow("x", "y", "first");
+         arrow_01.EmplaceArrow("f", "g", "second");
+
+         arrow_left.AddArrow(arrow_00);
+         arrow_right.AddArrow(arrow_01);
+
+         assert(!arrow_left.IsEquivalent(arrow_right));
+      }
    }
 }
 
